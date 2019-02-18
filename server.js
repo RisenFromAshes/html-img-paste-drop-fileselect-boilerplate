3b62fd4d89ca4abc538ef152fe7d819029174cc6
RisenFromAshes <ashrafurkhan37@gmail.com>
2019-02-19 05:29:47 +0600

    using multer

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const formidable = require('formidable')
 const uuidv1 = require('uuid/v1')
 const socketio = require('socket.io')
 
+const multer = require('multer')
+
+const imageUploadStorage = multer.diskStorage({
+    destination: (req,file,callback)=>{
+        callback(null,__dirname+'/multer_uploads')
+    },
+    filename: (req,file,callback)=>{
+        callback(null, uuidv1() + file.originalname)
+    }
+})
+const imageUpload = multer({storage: imageUploadStorage})
+const imageMiddleWare = imageUpload.fields([{name:'image', maxCount: Infinity}])
+
 const app = express()
 const server = app.listen(80)
 
@@ -28,37 +41,8 @@ app.get('/gallery', (req,res)=>{
     res.sendFile(__dirname + '/public/gallery.html')
 })
 
-app.post('/upload', (req, res) => {
-    //parsing the data as multipart form data
-
-    //only after file is uploaded // directly using parse results in incomplete files
-    new formidable.IncomingForm().parse(req)  
-    .on('file', (name, file)=>{ 
-        //getting the temporary file save location
-        var oldpath = file.path
-        //setting the new directory //directory has to be made prior to this
-        var newfilename = uuidv1() + file.name
-        var relpath = './uploads/' + newfilename
-        var newpath = __dirname + '/public/uploads/' + newfilename
-        //renaming which is the same as moving from one directory to another
-
-        //setting 10 second timeout just in case request fails
-        var timeout = setTimeout(()=>{
-            res.json({status:'error'})
-        }, 10000)
-
-        fs.rename(oldpath,newpath,(err)=>{
-            if(err) return res.json({status: 'error'})            
-            clearTimeout(timeout)
-            imgdata = {
-                src: relpath
-            }
-            imgdatas.push(imgdata)
-            io.emit('newimg', imgdata)
-            res.json({status:'success'})
-        })
-
-    })
+app.post('/upload', imageMiddleWare,(req, res) => {
+    res.json({status:'success'})
 })
 
 
